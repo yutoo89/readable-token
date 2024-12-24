5f448ea305c5ffbc1e3eeb02c26085ae43442231
yutoo89 <yuta.o.note@gmail.com>
2024-12-24 15:37:01 +0900

    create gem directories

---
+++ lib/readable/token.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "token/version"
+
+module Readable
+  module Token
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/readable/token_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Readable::Token do
+  it "has a version number" do
+    expect(Readable::Token::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
